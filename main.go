e1f0a32e85f87c2f30b6b2988bc22e53e6105bd2
“Jose <armasjose606@gmail.com>
2024-01-09 22:04:23 -0400

    Add: router and server

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func IndexRoute(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to my API")
+}
 
 func main() {
-	fmt.Println("Hello World")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", IndexRoute)
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
